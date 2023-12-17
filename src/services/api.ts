d70c6c26d4f2e5927fe67e1f5ec09bd813a0315b
Rafael Freire <rafaeldacostafreire@gmail.com>
2023-12-17 20:03:57 -0300

    refactor: :art: separation of calls to API or local data

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,36 @@
 import { Axios } from './axios';
 
 import { exportOnlyPriceValue } from '../helpers/monthlyPrices';
-import {
-  ALL_SYMBOLS_TIME_SERIES_MONTHLY,
-} from '../data/monthly_prices';
+import { ALL_SYMBOLS_TIME_SERIES_MONTHLY } from '../data/monthly_prices';
 import { SYMBOL_SEARCH, TIME_SERIES_MONTHLY } from '../constants/general';
 import { ALL_SYMBOL_SEARCH } from '@/data/ticker_search';
 
+// export async function getPricesInfo(symbol: string) {
+//   try {
+//     const { data } = await Axios.get(
+//       `query?function=${TIME_SERIES_MONTHLY}&symbol=${symbol}&apikey=${process.env.ALPHA_ADVANTAGE_API_KEY}`
+//     );
+
+//     return exportOnlyPriceValue(data['Monthly Time Series']);
+//   } catch (error) {
+//     return null;
+//   }
+// }
+
+// export async function getSearchBarSymbols(keyword: string) {
+//   try {
+//     const { data } = await Axios.get(
+//       `query?function=${SYMBOL_SEARCH}&keywords=${keyword}&apikey=${process.env.ALPHA_ADVANTAGE_API_KEY}`
+//     );
+
+//     return data.bestMatches;
+//   } catch (error) {
+//     return null;
+//   }
+// }
+
 export async function getPricesInfo(symbol: string) {
   try {
-    // const {data} = await Axios.get(`query?function=${TIME_SERIES_MONTHLY}&symbol=${symbol}&apikey=${process.env.ALPHA_ADVANTAGE_API_KEY}`);
-
-    // return exportOnlyPriceValue(data["Monthly Time Series"]);
-
     var data: any = '';
 
     ALL_SYMBOLS_TIME_SERIES_MONTHLY.map((ticket) => {
@@ -29,10 +47,6 @@ export async function getPricesInfo(symbol: string) {
 
 export async function getSearchBarSymbols(keyword: string) {
   try {
-    // const {data} = await Axios.get(`query?function=${SYMBOL_SEARCH}&keywords=${keyword}&apikey=${process.env.ALPHA_ADVANTAGE_API_KEY}`);
-
-    // return data.bestMatches;
-
     var data = ALL_SYMBOL_SEARCH.bestMatches;
 
     return data;
